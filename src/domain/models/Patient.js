764d31ee88346efae16a508f619726972c282687
Gustavo Leme <gleme@nevada.unr.edu>
2019-04-02 23:34:52 -0300

    First commit

---
+++ src/domain/models/Patient.js
@@ -0,0 +1,27 @@
+const { Model, STRING, INT } = require('sequelize');
+
+class Person extends Model {
+
+}
+
+Person.init({
+  cpf: {
+    type: INT,
+  },
+  name: {
+    type: STRING,
+    a
+  },
+  birthDate: {
+
+  },
+  gender: {
+
+  },
+  address: {
+
+  },
+  phoneNumber: {
+    
+  }
+})
