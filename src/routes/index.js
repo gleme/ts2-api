d33237b5387929b1eefb26cab44f468681868dcb
stould <vinirresko@gmail.com>
2019-04-04 18:21:37 -0300

    Rename www to www.js, add console rule, adjust tab-size = 4

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.status(200).json({ message: 'Hello World' });
+    res.status(200).json({ message: 'Hello World' });
 });
 
 module.exports = router;
