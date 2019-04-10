d139a91e5ff495c470cf9156e4f3fa4689a8d57a
Gustavo Leme <gleme@nevada.unr.edu>
2019-04-10 19:57:39 -0300

    add patient CRUD

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const patientRouter = require('./patient');
 const physicianRouter = require('./physician');
 
+router.use('/patient', patientRouter);
 router.use('/physician', physicianRouter);
 
-router.get('/', (req, res) => {
-    console.log(req.app.locals.mysqlDb);
+router.get('/ping', (req, res) => {
     res.status(200).end();
 });
 
