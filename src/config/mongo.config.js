c877fe625af2ddb63da9949e94f0602a4c67358b
Gustavo Leme <gleme@nevada.unr.edu>
2019-04-23 20:06:40 -0300

    inserts mongodb driver and updates npm audit fixes

---
+++ src/config/mongo.config.js
@@ -0,0 +1,19 @@
+const username = process.env.MONGODB_USERNAME;
+const password = process.env.MONGODB_PASSWORD;
+const database = process.env.MONGODB_DATABASE;
+const authDb = process.env.MONGODB_AUTHDB || 'admin';
+const host = process.env.MONGODB_HOST || 'localhost';
+const port = process.env.MONGODB_PORT || 27017;
+
+function getURI() {
+    return `mongodb://${username}:${password}@${host}:${port}/${database}`;
+}
+
+module.exports = {
+    getURI,
+    username,
+    database,
+    authDb,
+    host,
+    port
+};
