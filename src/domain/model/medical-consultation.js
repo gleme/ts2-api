c771b17a5a3db1c730454cf24b3fef8899a59303
Gustavo Leme <gleme@nevada.unr.edu>
2019-05-16 23:13:18 -0300

    tab space 2

--- src/domain/model/medical-consultation.js
+++ src/domain/model/medical-consultation.js
@@ -4,21 +4,21 @@ const { Patient } = require('./patient');
 const { MedicalProcedure } = require('./medical-procedure');
 
 class MedicalConsultation {
-    protocol: number;
-    date: Date;
-    prescription: string;
-    patient: Patient;
-    physician: Physician;
-    procedures: MedicalProcedure[];
+  protocol: number;
+  date: Date;
+  prescription: string;
+  patient: Patient;
+  physician: Physician;
+  procedures: MedicalProcedure[];
 
-    constructor(date: Date, prescription: string, patient: Patient, physician: Physician) {
-        this.date = date;
-        this.prescription = prescription;
-        this.patient = patient;
-        this.physician = physician;
-    }
+  constructor(date: Date, prescription: string, patient: Patient, physician: Physician) {
+    this.date = date;
+    this.prescription = prescription;
+    this.patient = patient;
+    this.physician = physician;
+  }
 }
 
 module.exports = {
-    MedicalConsultation: MedicalConsultation
+  MedicalConsultation: MedicalConsultation
 };
